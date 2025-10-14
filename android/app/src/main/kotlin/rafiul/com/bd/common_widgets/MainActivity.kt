6cab9df87ab340cb346cc8598ec5118e561bcc32
memahin <rafiul.mahin1@gmail.com>
2025-10-15 03:07:10 +0600

    Part 1

---
+++ android/app/src/main/kotlin/rafiul/com/bd/common_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package rafiul.com.bd.common_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Rupantorsoft"),),
+      body: Center(
+        child: Text("Hello World!"),
+      ),
+    );
+  }
+
+}
